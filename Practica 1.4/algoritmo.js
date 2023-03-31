117f544b5a0881bbe5a1473924928da33112d4cc
Practica 1.4 <122485403+antukung@users.noreply.github.com>
2023-03-30 22:10:40 -0300

    Add files via upload

---
+++ Practica 1.4/algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML=`
+<details>
+<summary>Titulo de primera</summary>
+<img height="200px" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU339lFcrbdFu6v1cf5WPwrgdu-L7BXk8Y9P8bkSdOaBcYl400wtk2mS_DnPbe1Wc0kWY&usqp=CAU" alt="perro">
+<iframe src="https://es.wikipedia.org/wiki/Wikipedia:Portada" frameborder="0"></iframe>
+<p>ESTE ES MI PARAFO POR FAVOR AYUUDAME EN MI VIDA SOY UN BUSCA</p>
+<a href="https://es-la.facebook.com/">facebook</a>
+
+</details>
+`;
